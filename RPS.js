6d46635c365ba5160c9f3e65af9675f33222ef64
ifteASC4 <ifteahmed2000@gmail.com>
2017-08-08 11:31:04 -0400

    rps highscore

--- RPS.js
+++ RPS.js
@@ -1,19 +1,43 @@
-var playerChoice = 'r';
-
+var playerRock = 'r';
+var playerPaper = 'p';
+var playerScissors = 's';
 var choices = ['r','p','s']; 
+
 var compChoice = choices[Math.floor(Math.random()*3)]; //random computer choices
 
-// tie situation
-if (playerChoice == compChoice){
+// rock situation
+if (playerRock == compChoice){
     console.log("It's a tie!");
+}else if (playerRock == 'r' && compChoice == 'p') {
+    console.log("Rocks loses to paper, You Lose!");
+}else (playerRock == 'r' && compChoice == 's') {
+    console.log("Rock wins against scissors, You Win!");
+}
+
+// paper situation
+if (playerPaper == compChoice) {
+    console.log("It's a tie!");
+} else if (playerPaper == 'p' && compChoice == 's') {
+    console.log("Paper loses to scissors, You Lose!");
+} else (playerPaper == 'p' && compChoice == 'r') {
+    console.log("Paper wins against rock, You Win!");
+}
+
+// scissors situation
+if (playerScissors == compChoice) {
+    console.log("It's a tie!");
+} else if (playerScissors == 's' && compChoice == 'r') {
+    console.log("Scissors loses to rock, You Lose!");
+} else (playerScissors == 's' && compChoice == 'p') {
+    console.log("Scissors wins against paper, You Win!");
 }
 
 // player picks rock computer picks paper
-if (playerChoice == 'r' && compChoice == 'p'){
-    console.log("Rocks loses to paper, You Lose!");
-}
+// if (playerChoice == 'r' && compChoice == 'p'){
+//     console.log("Rocks loses to paper, You Lose!");
+// }
 
 // player picks rock computer picks scissors 
-if (playerChoice == 'r' && compChoice == 's'){
-    console.log("Rock wins against scissors, You Win!");
-}
+// if (playerChoice == 'r' && compChoice == 's'){
+//     console.log("Rock wins against scissors, You Win!");
+// }
--- ifte-highscore/app.js
+++ ifte-highscore/app.js
@@ -1,3 +1,20 @@
+function playerChoosing() {
+    $(".rock").click(function () {
+        playerChoice = playerOptions[0]
+        hideTitle()
+    })
+    $(".paper").click(function () {
+        playerChoice = playerOptions[1]
+        hideTitle()
+        console.log(playerChoice)
+    })
+    $(".scissors").click(function () {
+        playerChoice = playerOptions[2]
+        hideTitle()
+    })
+}
+
+$(playerChoosing)
 //Set database object
 var database = firebase.database().ref();
 
@@ -23,25 +40,47 @@ database.on("child_added", function (rowData) {
 })
 
 var doa = Math.floor(Math.random() * 2)
-var statusOptions = ["Survived", "ded"];
+var statusOptions = ["Win", "Lose", "Tie"];
 var status = statusOptions[doa];
-
-function game() {
-    if (doa === 0) {
-        $("#outcome").append("You survived!");
-    }
-    if (doa === 1) {
-        $("#outcome").append("You died!");
-
-    }
-}
 
 // makes jumbotron go away
 function hideTitle() {
-    $(".btn").click(function () {
-        $(".jumbotron").hide()
-        game();
-    })
+    $(".jumbotron").hide()
+    game()
 }
-$(hideTitle);
 // end of the function that makes jumbotron go away
+
+var playerOptions = ['r', 'p', 's'];
+var options = ['r', 'p', 's'];
+
+var compOption = options[Math.floor(Math.random() * 3)]; //random computer choices
+
+function game() {
+    // rock situation
+    if (playerOptions == compOption) {
+        $("#outcome").append("Tie!")
+        status = statusOptions[2]
+    } else if ((playerChoice == "r") && compOption == 'p') {
+        $("#outcome").append("You lost!")
+        status = statusOptions[1]
+    } else if ((playerChoice == "r") && compOption == 's') {
+        $("#outcome").append("You won!")
+        status = statusOptions[0]
+    }
+    // paper situation
+    else if ((playerChoice == "p") && compOption == 's') {
+        $("#outcome").append("You lost!")
+        status = statusOptions[1]
+    } else if ((playerChoice == "p") && compOption == 'r') {
+        $("#outcome").append("You won!")
+        status = statusOptions[0]
+    }
+    // scissors situation
+    else if ((playerChoice == "s") && compOption == 'r') {
+        $("#outcome").append("You lost!")
+        status = statusOptions[1]
+    } else if ((playerChoice == "s") && compOption == 'p') {
+        $("#outcome").append("You won!")
+        status = statusOptions[0]
+    }
+}
